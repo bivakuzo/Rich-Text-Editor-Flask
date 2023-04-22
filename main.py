ae1c6833a0b139ee0e2cca0937a49fea15310180
Suraj Kumar <125875411+bivakuzo@users.noreply.github.com>
2023-04-22 08:08:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def index():
 with app.app_context():
     db.create_all()
 
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')    
-    # hosting at 0.0.0.0 for public otherwise will be localhost 
-    # and debugginf set to False
+#if __name__ == "__main__":
+    #app.run(debug=False, host='0.0.0.0')    
+    ## hosting at 0.0.0.0 for public otherwise will be localhost 
+    ## and debugginf set to False
